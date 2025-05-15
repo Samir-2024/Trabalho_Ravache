e6199beb2e6d69261624eb81fd16123514659fae
Samir <samir.chehade42@gmail.com>
2025-05-15 20:27:58 -0300

    Representacao No

---
+++ src/Arvore.java
@@ -0,0 +1,11 @@
+public class Arvore {
+    private No raiz;
+    private No filhos;
+    private No folha;
+
+    public Arvore() {
+        this.raiz = null;
+        this.folha = null;
+        this.filhos = null;
+    }
+}
